f78cf1de3cceca093d807d5193b496a5ea0dedbe
Jon Jagt <joj88658@bethel.edu>
2022-03-09 19:48:27 -0600

    Creates linkedlist.c file skeleton

---
+++ linkedlist.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <linkedlist.h>
+
+Value *makeNull() {
+
+}
+
+Value *cons(Value *car, Value *cdr) {
+
+}
+
+void display(Value *list) {
+
+}
+
+Value *car(Value *list) {
+
+}
+
+Value *cdr(Value *list) {
+
+}
+
+bool isNull(Value *value) {
+
+}
+
+int lenght(Value *value) {
+
+}
+
+Value *reverse(Value *list) {
+
+}
+
+void cleanup(Value *list) {
+
+}
